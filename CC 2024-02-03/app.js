6af781c34311b2df54577e64d62b41556a9ab81a
likhubhujee <nsforteen@gmail.com>
2024-02-03 11:45:30 +0545

    Adding coding challenger for 2024-02-03

---
+++ CC 2024-02-03/app.js
@@ -0,0 +1,11 @@
+// return true is sum of squares of array is greater than sum of cubes of array 
+
+function greaterThan(a, b){
+    let sumOfSquares = a.reduce((sum, num) => sum + num ** 2, 0)
+    let sumOfCubes = b.reduce((sum, num) => sum + num ** 3, 0)
+    return sumOfSquares > sumOfCubes
+}
+
+console.log(greaterThan([4,8,9], [1,2,3]), true)
+console.log(greaterThan([4,8,9], [8,4,9]), false)
+console.log(greaterThan([7,3,2], [1,4,2]), true)
